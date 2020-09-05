f6928733f3e5f003f1fc7b48753ac46c23f5f2aa
Alexey Filippov <sobsgm@gmail.com>
2020-09-05 18:47:26 +0300

    WIP

---
+++ service.install.js
@@ -0,0 +1,14 @@
+let Service = require('node-windows').Service;
+let svc = new Service({
+    name:'WBT.KKT.Service',
+    description: 'Service for Atol KKT',
+    script: 'C:\\KKT.Service\\bin\\www',
+    nodeOptions: [
+        '--harmony',
+        '--max_old_space_size=4096'
+    ]
+});
+svc.on('install',function(){
+    svc.start();
+});
+svc.install();
