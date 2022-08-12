c6bac16a7a9e5b55ef2ec95d41c2234e03891166
Alexey Filippov <sobsgm@gmail.com>
2022-08-12 14:13:23 +0300

    Update service.js

--- service.js
+++ service.js
@@ -4,7 +4,7 @@
  * Module dependencies.
  */
 
-let app = require('../app')
+let app = require('./app')
 let debug = require('debug')('kkt:server')
 let http = require('http')
 let axios = require('axios')
